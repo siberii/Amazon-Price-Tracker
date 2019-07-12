8aca451ef43f7b1ba14547f42580d369a6a2ab0c
Simon Berhanu <simon.berhanu@polymtl.ca>
2019-07-12 02:26:27 -0400

    Add console inputs

--- scraper.py
+++ scraper.py
@@ -1,13 +1,14 @@
 
+import getpass
+import time
+# Send emails (Simple Mail Transfer Protocol)
+import smtplib
+
 # Access a url and retrieve data from that website
 import requests
 # Parse retrived data and pull out individual items from it
 from bs4 import BeautifulSoup
-# Send emails (Simple Mail Transfer Protocol)
-import smtplib
 
-
-URL = "https://www.amazon.ca/Ripple-Junction-Doctor-Adult-T-Shirt/dp/B00U0HS8JI/ref=sr_1_5?keywords=t-shirt&qid=1562456707&s=pc&sr=1-5&th=1&psc=1"
 
 # Info on your browser
 headers = {
@@ -15,39 +16,62 @@ headers = {
 }
 
 
-def check_price():
+def check_price(sender, password, receiver, desired_price, URL):
+    price_matched = False
     # Return all data from URL
     page = requests.get(URL, headers=headers)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
     title = soup.find(id="productTitle").get_text().strip()
-    price = soup.find(id="priceblock_ourprice").get_text()
+    price = soup.find(id="priceblock_ourprice").get_text().strip()
+    print("\n Item: " + title + "\n Current Price: " + price)
 
-    converted_price = float(price[5:10])
+    converted_price = float(price[5:13].replace(',', ''))
 
-    if converted_price > 15:
-        send_email(title, converted_price)
+    if converted_price <= float(desired_price):
+        send_email(title, converted_price, URL, sender, password, receiver)
+        price_matched = True
+    return price_matched
 
 
-def send_email(title, price):
+def send_email(title, price, URL, sender, password, receiver):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
-    server.login("enter@email.here", "enterpasswordhere")
+    server.login(sender, password)
 
     subject = "Price went down!"
-    body = f"{title} went down to {price}$! Check the amazon link: {URL}"
+    body = f"{title} went down to {price}$!\nCheck the amazon link: {URL}\n"
 
     msg = f"Subject: {subject}\n\n{body}"
     server.sendmail(
-        "enter@sender.here", "enter@receiver.here", msg
+        sender, receiver, msg
     )
-    print("EMAIL HAS BEEN SENT!")
+    print("\nEMAIL HAS BEEN SENT!")
 
     server.quit()
 
 
-check_price()
+print("~ Welcome to Amazon Price Tracker! ~")
+sender = input("\nSender Email: ")
+try:
+    print("\n(Your password will be hidden.) ")
+    password = getpass.getpass()
+except Exception as error:
+    print('ERROR', error)
+receiver = input("\nReceiver Email: ")
+product_url = input("\nPaste Product URL: ")
+if product_url == "-":
+    product_url = "https://www.amazon.ca/Ripple-Junction-Doctor-Adult-T-Shirt/dp/B00U0HS8JI/ref=sr_1_5?keywords=t-shirt&qid=1562456707&s=pc&sr=1-5&th=1&psc=1"
+desired_price = input("\nDesired Price: ")
+frequency = input("\nPrice Check Frequency (seconds): ")
+
+price_matched = False
+while not price_matched:
+    price_matched = check_price(sender, password, receiver, desired_price,
+                                product_url)
+    if not price_matched:
+        time.sleep(int(frequency))
